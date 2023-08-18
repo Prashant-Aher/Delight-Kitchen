48de4ef5128c6abc72b29ad3038e3f651d72e2b4
Prashant-Aher <prashantaher2112@gmail.com>
2023-08-18 15:48:06 +0530

    backend template added

---
+++ backend_delight_kitchen/Delight-Kitchen/src/main/java/com/delightkitchen/DelightKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.delightkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelightKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelightKitchenApplication.class, args);
+	}
+
+}
---
+++ backend_delight_kitchen/Delight-Kitchen/src/test/java/com/delightkitchen/DelightKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delightkitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelightKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
