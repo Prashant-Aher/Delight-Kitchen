e2be1b867d8bf807f30a6e33189c0b5558cf8eb4
unknown <rakachaudhari1999@gmail.com>
2023-08-19 21:24:06 +0530

    Product dto added

---
+++ backend_delight_kitchen/DelightKitchen/src/main/java/com/delightkitchen/dtos/ProductDTOSend.java
@@ -0,0 +1,80 @@
+package com.delightkitchen.dtos;
+
+public class ProductDTOSend {
+	private int productId;
+	private String productName;
+	private float productPrice;
+	private String productStatus;
+	private int productcategoryId;
+	private String productcategoryName;
+
+	public ProductDTOSend() {
+
+	}
+
+	public ProductDTOSend(int productId, String productName, float productPrice, String productStatus,
+			int productcategoryId) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.productStatus = productStatus;
+		this.productcategoryId = productcategoryId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public float getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(float productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public String getProductStatus() {
+		return productStatus;
+	}
+
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
+	}
+
+	public int getProductcategoryId() {
+		return productcategoryId;
+	}
+
+	public void setProductcategoryId(int productcategoryId) {
+		this.productcategoryId = productcategoryId;
+	}
+
+	public String getProductcategoryName() {
+		return productcategoryName;
+	}
+
+	public void setProductcategoryName(String productcategoryName) {
+		this.productcategoryName = productcategoryName;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductDTOSend [productId=" + productId + ", productName=" + productName + ", productPrice="
+				+ productPrice + ", productStatus=" + productStatus + ", productcategoryId=" + productcategoryId
+				+ ", productcategoryName=" + productcategoryName + "]";
+	}
+
+}
