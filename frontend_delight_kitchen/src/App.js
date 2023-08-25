480f7eebda5800926241451f9cdde5319af12b07
Lokesh Choudhary <lokeshchoudhary2518@gmail.com>
2023-08-25 14:33:17 +0530

    User page Added in home page

--- frontend_delight_kitchen/src/App.js
+++ frontend_delight_kitchen/src/App.js
@@ -4,38 +4,30 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home';
 import Login from './pages/Login';
-<<<<<<< HEAD
 import User from './pages/User';
-=======
 import Logout from './pages/Logout';
 
->>>>>>> 4eff32b9b7d4babdf0eae188115e3fc5773f7ff0
 
-
-const AuthorizeUser = () => {
+  const AuthorizeUser = () => {
   const loginStatus = sessionStorage['loginStatus']
   return loginStatus == '1' ? <Home /> : <Login />
 }
 
-const AuthorizeLogin = () => {
+  const AuthorizeLogin = () => {
   const loginStatus = sessionStorage['loginStatus']
   return loginStatus == '1' ? <Home /> : <Login />
 }
 
-const AuthorizeHome = () => {
+  const AuthorizeHome = () => {
   const loginStatus = sessionStorage['loginStatus']
-<<<<<<< HEAD
-  return loginStatus == '1' ? <Home /> : <Home/>
+  return loginStatus == '1' ? <Home /> : <Login/>
 }
 
 const AuthorizeUsers = () => {
   const loginStatus = sessionStorage['loginStatus']
   return loginStatus == '1' ? ((sessionStorage['role']=='manager')?<User />:<Home/> ) : <Login />
-=======
-  return loginStatus == '1' ? <Home /> : <Home />
-
->>>>>>> 4eff32b9b7d4babdf0eae188115e3fc5773f7ff0
 }
+
 const AuthorizeLogout = () => {
   const loginStatus = sessionStorage['loginStatus']
   return loginStatus == '1' ? <Logout /> : <Login />
@@ -50,12 +42,8 @@ function App() {
           <Route path="/" element={<AuthorizeUser />} />
           <Route path="/login" element={<AuthorizeLogin />} />
           <Route path="/home" element={<AuthorizeHome />}/>
-<<<<<<< HEAD
           <Route path="/users" element={<AuthorizeUsers />} />
-
-=======
           <Route path="/logout" element={<AuthorizeLogout />} />
->>>>>>> 4eff32b9b7d4babdf0eae188115e3fc5773f7ff0
         </Routes>
       </BrowserRouter>
       <ToastContainer theme="colored" />
