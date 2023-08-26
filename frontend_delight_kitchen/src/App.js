a460a04dc80e82238beed2f87879d671f00eab8d
Lokesh Choudhary <lokeshchoudhary2518@gmail.com>
2023-08-26 12:16:05 +0530

    Setting page is ready

--- frontend_delight_kitchen/src/App.js
+++ frontend_delight_kitchen/src/App.js
@@ -2,13 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './css/SideBar.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
-import Home from './pages/Home';
 import Login from './pages/Login';
+import Home from './pages/Home';
 import User from './pages/User';
-import Logout from './pages/Logout';
 import Bill from './pages/Bill';
 import Category from './pages/Category';
-
+import Setting from './pages/Setting';
+import Logout from './pages/Logout';
 
   const AuthorizeUser = () => {
   const loginStatus = sessionStorage['loginStatus']
@@ -30,19 +30,25 @@ const AuthorizeUsers = () => {
   return loginStatus == '1' ? ((sessionStorage['role']=='manager')?<User />:<Home/> ) : <Login />
 }
 
-const AuthorizeLogout = () => {
-  const loginStatus = sessionStorage['loginStatus']
-  return loginStatus == '1' ? <Logout /> : <Login />
-}
-
 const AuthorizeBill = () => {
   const loginStatus = sessionStorage['loginStatus']
   return loginStatus == '1' ? ((sessionStorage['role']=='manager'||sessionStorage['role']=='cashier')?<Bill />:<Home/> ) : <Login />
 }
 
+
 const AuthorizeCategory = () => {
   const loginStatus = sessionStorage['loginStatus']
   return loginStatus == '1' ? ((sessionStorage['role']=='manager'||sessionStorage['role']=='chef')?<Category />:<Home/> ): <Login />
+}
+
+const AuthorizeSetting = () => {
+  const loginStatus = sessionStorage['loginStatus']
+  return loginStatus == '1' ? <Setting /> : <Login />
+}
+
+const AuthorizeLogout = () => {
+  const loginStatus = sessionStorage['loginStatus']
+  return loginStatus == '1' ? <Logout /> : <Login />
 }
 
 function App() {
@@ -54,10 +60,11 @@ function App() {
           <Route path="/login" element={<AuthorizeLogin />} />
           <Route path="/home" element={<AuthorizeHome />}/>
           <Route path="/users" element={<AuthorizeUsers />} />
-          <Route path="/logout" element={<AuthorizeLogout />} />
           <Route path="/bill" element={<AuthorizeBill />} />
           <Route path="/category" element={<AuthorizeCategory />} />
-          
+          <Route path="/setting" element={<AuthorizeSetting/>} />
+          <Route path="/logout" element={<AuthorizeLogout />} />
+       
         </Routes>
       </BrowserRouter>
       <ToastContainer theme="colored" />
